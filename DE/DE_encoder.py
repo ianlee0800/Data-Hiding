5ec569aad0201998d21d76213987ef9ec41f637c
ianlee0800 <ianlee1212@gmail.com>
2023-12-03 18:46:26 +0800

    Complete?

--- DE/DE_encoder.py
+++ DE/DE_encoder.py
@@ -2,13 +2,29 @@ import cv2
 import numpy as np
 from skimage.metrics import structural_similarity as ssim
 import os
+import random
 
 # Constants
+channel_names = ['R', 'G', 'B']
 FILE_TYPE = "png"
 DE_IMAGES_PATH = "./DE/images/"
 DE_MARKED_PATH = "./DE/marked/"
 LOCATION_MAP_PATH = "./DE/location_map/"
 LSB_PATH = "./DE/lsb/"
+EMBED_DATA_PATH = "./DE/embed_data"
+
+def save_embedded_data_as_text(img_name, data_to_hide, embedded_payload_size):
+    with open(f"{EMBED_DATA_PATH}/{img_name}_embed_data.txt", "w") as file:
+        file.write(''.join(map(str, data_to_hide[:embedded_payload_size])))
+
+def save_embedded_data_as_text_color(img_name, data_to_hide, channel_payload_sizes):
+    with open(f"{EMBED_DATA_PATH}/{img_name}_embed_data.txt", "w") as file:
+        for c in range(3):
+            payload_size = int(channel_payload_sizes[c])  # 确保为整数
+            channel_data = data_to_hide[:payload_size]
+            file.write(''.join(map(str, channel_data)))
+            if c < 2:  # 在通道数据之间添加分隔符（如果需要）
+                file.write("\n")
 
 def calculate_psnr(original, marked):
     if len(original.shape) == 3 and len(marked.shape) == 2:
@@ -76,9 +92,19 @@ def embed_data_in_difference(h, bit):
         h = 2 * h
     return h
 
+def generate_random_payload(payload_size, ratio_of_ones):
+    num_ones = int(payload_size * ratio_of_ones)
+    num_zeros = payload_size - num_ones
+
+    payload = [1] * num_ones + [0] * num_zeros
+    random.shuffle(payload)
+    return payload
+
 def is_expandable(h, l):
-    # 確保 h 和 l 是單個數字
-    return abs(2 * h + 1) <= min(2 * (255 - l), 2 * l + 1) and \
+    if h == 0 or h == 1:
+        return False
+    else:
+        return abs(2 * h + 1) <= min(2 * (255 - l), 2 * l + 1) and \
            abs(2 * h) <= min(2 * (255 - l), 2 * l + 1)
 
 def is_changeable(h, l):
@@ -86,68 +112,66 @@ def is_changeable(h, l):
     return abs(2 * (h // 2) + 1) <= min(2 * (255 - l), 2 * l + 1) and \
            abs(2 * (h // 2)) <= min(2 * (255 - l), 2 * l + 1)
 
-def hide_data_in_grayscale_channel(channel, data, location_map, lsb, debug_file_path):
-    with open(debug_file_path, 'w') as debug_file:
-        if len(channel.shape) == 3:
-            channel = cv2.cvtColor(channel, cv2.COLOR_BGR2GRAY)
+def hide_data_in_grayscale_channel(channel, data, location_map, original_lsb):
+    if len(channel.shape) == 3:
+        channel = cv2.cvtColor(channel, cv2.COLOR_BGR2GRAY)
 
-        height, width = channel.shape
-        i = 0
-        for y in range(0, height, 2):
-            for x in range(0, width - 1, 2):
-                if i >= len(data):
-                    break
+    height, width = channel.shape
+    i = 0
+    for y in range(height):
+        for x in range(0, width, 2):
+            if i >= len(data):
+                break
 
-                pixel1 = channel[y, x]
-                pixel2 = channel[y, x + 1]
-                l, h = integer_transform(pixel1, pixel2)
+            pixel1 = channel[y, x]
+            pixel2 = channel[y, x + 1] if x + 1 < width else pixel1  # 处理最后一列像素
 
-                # 检查是否可扩展
-                is_expandable = abs(2 * h + data[i]) <= min(2 * (255 - l), 2 * l + 1)
-                
-                if is_expandable:
-                    location_map.append(1)  # 标记为用于嵌入
-                    h = 2 * h + data[i]
-                    i += 1
-                else:
-                    location_map.append(0)  # 标记为未使用
+            l, h = integer_transform(pixel1, pixel2)
+            original_lsb[y, x] = pixel1 % 2
 
-                x_new, y_new = inverse_integer_transform(l, h)
-                channel[y, x], channel[y, x + 1] = x_new, y_new
+            if is_expandable(h, l):
+                location_map[y, x] = 1  # 标记为用于嵌入
+                h = embed_data_in_difference(h, data[i])
+                i += 1
+            else:
+                location_map[y, x] = 0  # 标记为未使用
 
-        # 更新Payload size
-        updated_payload_size = sum(location_map)
-        return channel, updated_payload_size
+            x_new, y_new = inverse_integer_transform(l, h)
+            channel[y, x] = x_new
+            if x + 1 < width:
+                channel[y, x + 1] = y_new
 
-def hide_data_in_color_channel(channel, data, location_map, lsb, debug_file_path):
-    with open(debug_file_path, 'w') as debug_file:
-        # 单个颜色通道的图像，所以只有两个维度
-        height, width = channel.shape
-        i = 0
-        for y in range(0, height, 2):
-            for x in range(0, width - 1, 2):
-                if i >= len(data):
-                    break
+    updated_payload_size = np.sum(location_map)
+    return channel, updated_payload_size
 
-                pixel1 = channel[y, x]
-                pixel2 = channel[y, x + 1]
-                l, h = integer_transform(pixel1, pixel2)
-                debug_file.write(f"Pixel1: {pixel1}, Pixel2: {pixel2}, l: {l}, h: {h}\n")
+def hide_data_in_color_channel(channel, data, location_map, original_lsb):
+    height, width = channel.shape
+    i = 0
+    for y in range(height):
+        for x in range(0, width, 2):
+            if i >= len(data):
+                break
 
-                original_lsb = pixel1 % 2
-                lsb.append(original_lsb)
+            pixel1 = channel[y, x]
+            pixel2 = channel[y, x + 1] if x + 1 < width else pixel1  # 处理最后一列像素
 
-                if is_expandable(h, l):
-                    location_map.append(1)  # 标记为用于嵌入
-                    h = 2 * h + data[i]
-                    i += 1
-                else:
-                    location_map.append(0)  # 标记为未使用
+            l, h = integer_transform(pixel1, pixel2)
+            original_lsb[y, x] = pixel1 % 2
 
-                channel[y, x], channel[y, x + 1] = inverse_integer_transform(l, h)
-        
-        updated_payload_size = sum(location_map)
-        return channel, updated_payload_size
+            if is_expandable(h, l):
+                location_map[y, x] = 1  # 标记为用于嵌入
+                h = embed_data_in_difference(h, data[i])
+                i += 1
+            else:
+                location_map[y, x] = 0  # 标记为未使用
+
+            x_new, y_new = inverse_integer_transform(l, h)
+            channel[y, x] = x_new
+            if x + 1 < width:
+                channel[y, x + 1] = y_new
+
+    updated_payload_size = np.sum(location_map)
+    return channel, updated_payload_size
 
 def calculate_max_payload_size(image):
     height, width = image.shape[:2]
@@ -159,46 +183,42 @@ def main():
 
     if orig_img is None:
         print("Error reading original image file")
-        exit()
-
+        return
+    
+    height, width = orig_img.shape[:2]
     grayscale = is_grayscale(orig_img)
     max_payload_size = calculate_max_payload_size(orig_img)
-    data_to_hide = [1] * max_payload_size
-    location_map = []
-    original_lsb = []
 
-    debug_file_path = "./DE/debug_info.txt"  # 调试信息文件路径
-    channel_names = ['R', 'G', 'B']  # 用于命名文件的通道名
+    # 生成随机有效载荷数据
+    ratio_of_ones = 0.5  # 这里可以根据需要调整 1 的比例，舉例來說 ratio_of_ones = 0.5 表示 50% 的 1 和 50% 的 0
+    data_to_hide = generate_random_payload(max_payload_size, ratio_of_ones)
+
+    location_map = np.zeros((height, width), dtype=int)
+    original_lsb = np.zeros((height, width), dtype=int)
 
     if grayscale:
         print(f"{img_name} is a Grayscale image")
-        marked_img, updated_payload_size = hide_data_in_grayscale_channel(orig_img.copy(), data_to_hide, location_map, original_lsb, debug_file_path)
+        marked_img, updated_payload_size = hide_data_in_grayscale_channel(orig_img.copy(), data_to_hide, location_map, original_lsb)
+        save_embedded_data_as_text(img_name, data_to_hide, updated_payload_size)        
+        np.save(LOCATION_MAP_PATH + f"{img_name}_location_map.npy", location_map)
+        np.save(LSB_PATH + f"{img_name}_original_lsb.npy", original_lsb)
+
     else:
         print(f"{img_name} is a Color image")
         marked_img = orig_img.copy()
-        location_maps = [[], [], []]
-        original_lsbs = [[], [], []]
-        total_payload_size = 0  # 用于累加彩色图像的payload size
+        location_maps = [np.zeros((height, width)) for _ in range(3)]
+        original_lsbs = [np.zeros((height, width)) for _ in range(3)]
 
+        channel_payload_sizes = []
         for c in range(3):
             channel = marked_img[:, :, c]
-            channel_location_map = []
-            channel_lsb = []
-            marked_channel, channel_payload_size = hide_data_in_color_channel(channel, data_to_hide, channel_location_map, channel_lsb, debug_file_path)
+            marked_channel, channel_payload_size = hide_data_in_color_channel(channel, data_to_hide, location_maps[c], original_lsbs[c])
             marked_img[:, :, c] = marked_channel
-            location_maps[c] = channel_location_map
-            original_lsbs[c] = channel_lsb
-            total_payload_size += channel_payload_size
+            channel_payload_sizes.append(channel_payload_size)
 
-        updated_payload_size = total_payload_size  # 更新总payload size
-
-    # Save location map and original LSBs
-    create_directory_if_not_exists(LOCATION_MAP_PATH)
-    create_directory_if_not_exists(LSB_PATH)
-    if grayscale:
-        np.save(LOCATION_MAP_PATH + f"{img_name}_location_map.npy", location_map)
-        np.save(LSB_PATH + f"{img_name}_original_lsb.npy", original_lsb)
-    else:
+        save_embedded_data_as_text_color(img_name, data_to_hide, channel_payload_sizes)
+        
+        updated_payload_size = sum(channel_payload_sizes)
         for c in range(3):
             np.save(LOCATION_MAP_PATH + f"{img_name}_channel_{channel_names[c]}_location_map.npy", location_maps[c])
             np.save(LSB_PATH + f"{img_name}_channel_{channel_names[c]}_original_lsb.npy", original_lsbs[c])
@@ -206,8 +226,8 @@ def main():
     cv2.imwrite(DE_MARKED_PATH + f"{img_name}_markedImg.{FILE_TYPE}", marked_img)
     
     # Calculate and display metrics
-    print(f"Payload size: {updated_payload_size}")
-    print(f"Payload content: {''.join(map(str, data_to_hide[:50]))}...")  # Print first 50 bits for brevity
+    print(f"Embedded Payload size: {updated_payload_size}")
+    print(f"Payload content: {''.join(map(str, data_to_hide[:50]))}...")
 
     bpp = updated_payload_size / (orig_img.shape[0] * orig_img.shape[1])
     print(f"Bits per pixel (bpp): {bpp:.4f}")
@@ -228,5 +248,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
